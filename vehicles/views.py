33e52d1b06795e6b6c03da07664eea66f736dc23
misto <ottonellicopattibruno@gmail.com>
2023-12-04 00:27:01 -0300

    Set login & register urls

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from .models import User
+
 def index(request: HttpRequest):
     return render(request, "vehicles/index.html")
 
 
+def login_view(request: HttpRequest):
+    pass
+
+
+def register_view(request: HttpRequest):
+    if request.method == "POST":
+        return HttpResponse("TODO")
+
+    # Any other method will render the form
+    return render(request, "vehicles/register.html")
