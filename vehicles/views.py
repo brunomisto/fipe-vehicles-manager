204044b4d82320c01b78ecc4a5f55edf782a8777
misto <ottonellicopattibruno@gmail.com>
2023-12-03 23:36:58 -0300

    Add and configure User model

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse("<h1>Vehicles</h1>")
